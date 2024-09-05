06be2c0163950427b50d360d09f0841a22dbec0c
Richard Guedes <richard.silva46@fatec.sp.gov.br>
2024-09-05 06:11:59 -0300

    Build: Change and separate backend and frontend repository

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import "./assets/css/App.css";
+import React, { useContext, useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Chart from "./Components/Chart/Chart";
+import Home from "./Pages/Home";
+import Trade from "./Pages/Trade";
+import OldTransactions from "./Pages/OldTransactions";
+import OpenTrades from "./Pages/OpenTrades";
+import Config from "./Pages/Config";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import {
+  UserContext,
+  UserDataProvider,
+  useUserData,
+} from "./Services/UserDataProvider";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+function App() {
+  useEffect(() => {
+  
+  }, []);
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCcPW_2CU-lfts53hVgYKi7YmgIDAzK9S4",
+    authDomain: "tharseo-c8cb9.firebaseapp.com",
+    projectId: "tharseo-c8cb9",
+    storageBucket: "tharseo-c8cb9.appspot.com",
+    messagingSenderId: "911731612364",
+    appId: "1:911731612364:web:f59461e2e5b12cc6080069",
+    measurementId: "G-HJDWVMTTD0"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  return (
+    <UserDataProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route
+              path="/home"
+              element={
+                <Home
+                     />
+              }
+            />
+            <Route
+              path="/trade"
+              element={
+                <Trade  />
+              }
+            />
+            <Route
+              path="/oldtransactions"
+              element={
+                <OldTransactions
+                 
+                />
+              }
+            />
+            <Route
+              path="/opentrades"
+              element={
+                <OpenTrades
+                
+                />
+              }
+            />
+            <Route
+              path="/config"
+              element={
+                <Config
+               
+                />
+              }
+            />
+            <Route path="/chart" element={<Chart />} />
+            <Route
+              exact
+              path="/"
+              element={
+                <Login
+                 
+                />
+              }
+            />
+            <Route
+              path="/register"
+              element={<Register />}
+            />
+            {/* <Route path="/" element={""} /> */}
+          </Routes>
+        </div>
+      </Router>
+    </UserDataProvider>
+  );
+}
+
+export default App;

92bdcddb4f9d629c52a9d176e113c3ee2703cffa
Richard Guedes <richard.silva46@fatec.sp.gov.br>
2024-10-06 22:14:05 -0300

    Feat : Implemented partial google auth and zustand

--- src/Services/ServerConfig.js
+++ src/Services/ServerConfig.js
@@ -1,6 +1,6 @@
 const serverConfig = {
-  addressServerTharseo: "http://localhost:8080",
-  // addressServerTharseo: "https://tharseo.zapto.org:443/authenticate/auth:443",
+  // addressServerTharseo: "http://localhost:8080",
+  addressServerTharseo: "https://tharseo.zapto.org:443",
 };
 
 export default serverConfig;
